5fc804df4047263a9d99c630ba43d1e6c0252f16
Mohamed Soubhi <Eng.mohamed.soubhi@gmail.com>
2026-02-14 00:32:29 +0100

    fixing the cross compiler CROSS_COMPILE

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,7 +11,7 @@ KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE=aarch64-linux-gnu-
 
 if [ $# -lt 1 ]
 then
